7ea85ea061327036bb079bbdb42cc4f271a63149
Sravani <sravanidittakavi99@gmail.com>
2026-01-13 22:47:18 +0530

    question2

---
+++ question-2/index.js
@@ -0,0 +1,9 @@
+
+
+import { checkPrime } from "./math.js";
+
+console.log(checkPrime(2));   
+console.log(checkPrime(7));   
+console.log(checkPrime(10));  
+console.log(checkPrime(17));  
+console.log(checkPrime(20));  
